2b9688443fa67884f7dd2e13623e86f2fcca0b06
DaniloMicael <danilomc2004@gmail.com>
2024-10-14 12:48:01 +0100

    Simple refactor

--- student.py
+++ student.py
@@ -4,6 +4,37 @@ import json
 import os
 import websockets
 
+def get_next_move(state):
+    
+    # extract information from the state
+    level = state["level"]  # get the level (provavelmente não é necessário -----------)
+    step = state["step"]  # get the step (provavelmente não é necessário -----------)
+    timeout = state["timeout"]  # get the timeout (provavelmente não é necessário -----------)
+    
+    food_position = state["food"][0][0:2]  # get the food position
+    food_type = state["food"][0][2]  # get the food type
+    
+    timestamp = state["ts"]  # get the timestamp (provavelmente não é necessário -----------)
+    name = state["name"]  # get the snake name (provavelmente não é necessário -----------)
+    
+    snake_body = state["body"]  # get the snake body positions
+    snake_head = snake_body[0]  # get the snake head position
+    snake_tail = snake_body[-1]  # get the snake tail position
+    snake_sight = state["sight"]  # get the snake sight
+    score = state["score"]  # get the score
+    snake_range = state["range"]  # get the snake range
+    snake_traverse = state["traverse"]  # get the snake traverse
+    
+    # simple logic to move the snake to the food
+    if snake_head[0] < food_position[0]:
+        return "d" # move right
+    elif snake_head[0] > food_position[0]:
+        return "a" # move left
+    elif snake_head[1] < food_position[1]:
+        return "s" # move down
+    elif snake_head[1] > food_position[1]:
+        return "w" # move up
+
 async def agent_loop(server_address="localhost:8000", agent_name="student"):
     async with websockets.connect(f"ws://{server_address}/player") as websocket:
         # Receive information about static game properties
@@ -18,36 +49,7 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                 state = json.loads(await websocket.recv())  # receive game update, this must be called timely or your game will get out of sync with the server
                 print(state)
                 
-                # extract information from the state
-                level = state["level"]  # get the level (provavelmente não é necessário -----------)
-                step = state["step"]  # get the step (provavelmente não é necessário -----------)
-                timeout = state["timeout"]  # get the timeout (provavelmente não é necessário -----------)
-                
-                food_position = state["food"][0][0:2]  # get the food position
-                food_type = state["food"][0][2]  # get the food type
-                
-                timestamp = state["ts"]  # get the timestamp (provavelmente não é necessário -----------)
-                name = state["name"]  # get the snake name (provavelmente não é necessário -----------)
-                
-                snake_body = state["body"]  # get the snake body positions
-                snake_head = snake_body[0]  # get the snake head position
-                snake_tail = snake_body[-1]  # get the snake tail position
-                snake_sight = state["sight"]  # get the snake sight
-                score = state["score"]  # get the score
-                snake_range = state["range"]  # get the snake range
-                snake_traverse = state["traverse"]  # get the snake traverse
-                
-                key = ""
-                
-                # simple logic to move the snake to the food
-                if snake_head[0] < food_position[0]:
-                    key = "d" # move right
-                elif snake_head[0] > food_position[0]:
-                    key = "a" # move left
-                elif snake_head[1] < food_position[1]:
-                    key = "s" # move down
-                elif snake_head[1] > food_position[1]:
-                    key = "w" # move up
+                key = get_next_move(state)
 
                 await websocket.send(
                     json.dumps({"cmd": "key", "key": key})
