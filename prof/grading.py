a3e37a4f46f94c561732bed708c400bc1f6be8ba
Ubuntu <dgomes@atnog-tetriscores.av.it.pt>
2024-10-21 22:50:21 +0100

    running

--- prof/grading.py
+++ prof/grading.py
@@ -140,4 +140,4 @@ if __name__ == "__main__":
         with app.app_context():
             db.create_all()
 
-    app.run(debug=False, host="0.0.0.0", port=9000)
+    app.run(debug=False, host="0.0.0.0", port=80)
